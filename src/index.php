2e51ddf6ac213d02834f0dd53523991aad218a6b
Malibu <malibu1106@gmail.com>
2024-08-26 09:54:01 +0200

    first_commit

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/script.js" defer></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Pizza</title>
+</head>
+
+<body>
+<h1>Pizza</h1>
+</body>
+
+</html>
